7fbd88dd23439db3dd4dafd9b3750ca9b1b2785e
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-06 21:01:40 +0200

    Add flex-column

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar () {
     return (
-        <nav>
-            <div>
+        <nav >
+            <div class="flex flex-col">
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
             </div>
