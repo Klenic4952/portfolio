b4863bad69dc433ea5ce247400f8e5def6a31f6e
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-10 16:58:01 +0200

    Change style

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Navbar () {
     return (
         <nav >
-            <div class="flex flex-col">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
+            <div class="flex flex-col mt-8">
+                <Link href="/" class="font-bold mb-3">HOME</Link>
+                <Link href="/about" class="font-bold">ABOUT</Link>
             </div>
         </nav>
     )
