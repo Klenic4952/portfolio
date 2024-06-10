8ad934dbce3e4cf3a149c69291ddca2491c35625
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-10 16:57:11 +0200

    Add text

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,12 @@
 export default function About() {
-    return (
-        <main>
-            <h2>About</h2>
-        </main>
-    )
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        {`I am pioneering as a front-end developer. Having just completed my
+          front-end development training at Winc Academy, this portfolio is now
+          my first real "personal project".`}
+      </p>
+    </main>
+  );
 }
