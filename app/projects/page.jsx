6b60d21590740bc8fa97719e41f9a51a343947c1
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-12 16:31:49 +0200

    Create section Projects

---
+++ app/projects/page.jsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+    return (
+      <div class="bg-[#1B0F1C] p-10 rounded-xl">
+        <h2>PROJECTS</h2>
+        <p class="text-[#987E9E]">
+          MY PROJECTS
+        </p>
+      </div>
+    );
+  }
