e0a8b7486aa380eef01a57b76cfd080ac665d19b
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-12 16:37:18 +0200

    Change Flex

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <div class="flex flex-col max-w-96">
-      <h1 class="mb-3">Nicolien Klein</h1>
-      <h2 class="text-midpurple text-2xl mb-4">Junior Front-End Developer</h2>
-      <p class="text-lg">Pioneering with this portfolio, build with next.js.</p>
-
-      <Navbar />
-    </div>
+    <header class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg: justify-between lg:py-24">
+      <div>
+        <h1 class="mb-3">
+          <Link href="/">Nicolien Klein</Link>
+        </h1>
+        <h2 class="text-midpurple text-2xl mb-4">Junior Front-End Developer</h2>
+        <p class="text-lg">
+          Pioneering with Next.js and Tailwind to build this portfolio. 
+        </p>
+        <Navbar />
+      </div>
+    </header>
   );
 }
