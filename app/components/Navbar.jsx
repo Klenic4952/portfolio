bdbbd3295107c078c35de9d2bce04f3e5677b049
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-12 16:36:32 +0200

    Change Navbar links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 
-export default function Navbar () {
-    return (
-        <nav >
-            <div class="flex flex-col mt-8">
-                <Link href="/" class="font-bold mb-3">HOME</Link>
-                <Link href="/about" class="font-bold">ABOUT</Link>
-            </div>
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav class=" lg:block" aria-label="In-page-jump-links">
+      <ul class="mt-16 w-max">
+        <li>
+          <Link href="/about" class="group flex items-center py-3 active">
+           ABOUT
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects" class="font-bold">
+            PROJECTS
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
