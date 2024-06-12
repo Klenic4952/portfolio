b36e61937acdac1052d1ced01c5628122c6332d4
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-12 16:37:42 +0200

    Add bg-color

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,12 @@
 export default function About() {
   return (
-    <main>
+    <div class="bg-[#DBAA78] p-10 rounded-xl">
       <h2>About</h2>
       <p>
         {`I am pioneering as a front-end developer. Having just completed my
           front-end development training at Winc Academy, this portfolio is now
           my first real "personal project".`}
       </p>
-    </main>
+    </div>
   );
 }
