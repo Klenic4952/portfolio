a56978c3c158aa2792e520c518152e7682ab2488
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-12 16:41:10 +0200

    Add section projects

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,17 @@
 // app/home.jsx is the UI for the '/' URL
 
 import About from "./about/page";
-
+import Projects from "./projects/page";
 
 export default function Home() {
   return (
-    <div >
-      <main>
-        <section>
-          <About/>
-        </section>
-
-      </main>
-    </div>
+    <main class="pt-24 lg:w-1/2 lg:py-24">
+      <section class="my-10">
+        <About />
+      </section>
+      <section>
+        <Projects />
+      </section>
+    </main>
   );
 }
