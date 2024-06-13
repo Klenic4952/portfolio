81378d7c4c11f5a797f2f969db0600d0109e675f
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-13 17:34:26 +0200

    Add scroll

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Projects from "./projects/page";
 export default function Home() {
   return (
     <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
-      <section id="about" class="my-10">
+      <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="about me">
         <About />
       </section>
       <section id="projects">
