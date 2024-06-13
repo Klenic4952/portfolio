06aa225bd448155ba8cc7dfb38ab9e314c36c622
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-13 17:32:15 +0200

    Change to Dutch

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,9 @@
 export default function Projects() {
     return (
       <div class="bg-[#1B0F1C] p-10 rounded-xl">
-        <h2>PROJECTS</h2>
+        <h2>PROJECTEN</h2>
         <p class="text-[#987E9E]">
-          MY PROJECTS
+          PROJECTEN
         </p>
       </div>
     );
