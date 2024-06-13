004a0ca8d413d9fd3a3e798c9c13ccff9ad0a4dc
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-13 17:35:32 +0200

    Create fixed header

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,17 +10,21 @@ const rubik = Rubik({ subsets: ["latin"] });
 export const metadata = {
   title: "Nicolien Klein",
   description:
-    "Nicolien Klein is pioneering as a front-end webdeveloper, with love for nice color combinations",
+    "Nicolien Klein is aan het pionieren als front-end webdeveloper, met speciale interesse voor leuke kleurencombinaties",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" class="scroll-smooth">
       <body className={rubik.className}>
-        <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-          <div class="lg:flex lg:justify-between lg:gap-4">
-            <Header />
-            {children}
+        <div class="relative">
+          <div class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute">
+          </div>
+          <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+            <div class="lg:flex lg:justify-between lg:gap-4">
+              <Header />
+              {children}
+            </div>
           </div>
         </div>
       </body>
