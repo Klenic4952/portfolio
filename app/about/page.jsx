fe90ca28b6b80a896a696ec57a04a1c0f68f4455
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-13 17:33:15 +0200

    Change to Dutch

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,11 +1,34 @@
 export default function About() {
   return (
-    <div class="bg-[#DBAA78] p-10 rounded-xl">
+    <div class="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20">
       <h2>About</h2>
-      <p>
-        {`I am pioneering as a front-end developer. Having just completed my
-          front-end development training at Winc Academy, this portfolio is now
-          my first real "personal project".`}
+      <p class="mb-4">
+        Na een loopbaan van bijna 25 jaar bij de Rabobank was het tijd voor wat
+        anders. Ik startte mijn eigen praktijk als voedingscoach. Met behulp van
+        WordPress maakte ik mijn eigen website. Dit wekte een nieuwe interesse
+        tot leven. Voor ik het wist had ik een opleiding tot Front-End
+        Development geboekt bij Winc Academy.
+      </p>
+      <p class="mb-4">
+        Nadat ik de opleiding in mei 2024 had afgerond, heb ik dit portfolio
+        gemaakt. Op dit moment werk ik aan een nieuwe website voor mijn
+        praktijk. Zodra deze klaar is, voeg ik deze natuurlijk ook toe aan mijn
+        portfolio.Op dit moment besteed ik bij het maken van websites altijd
+        veel aandacht aan het kleurgebruik. Ik maak daarbij bijvoorbeeld gebruik
+        van http://colormind.io/ en https://coolors.co/.
+      </p>
+      <p class="mb-4">
+        Zowel voeding als coderen en tech in het algemeen hebben mijn bijzondere
+        interesse. Ik ben benieuwd waar deze interesses me verder nog gaan
+        brengen.
+      </p>
+      <p class="mb-4">
+        Tijd om de hele dag achter de computer te zitten heb ik ook weer niet.
+        Ik run ook nog een ander bedrijf: mijn gezin. Met een man met een eigen
+        bedrijf, twee tieners en een kat: never al dull moment. Ik houd van de
+        natuur en daar wonen we dichtbij. Ik wandel graag, dat is een fijne
+        combinatie om lekker te bewegen en te ontspannen. En lekker eten niet te
+        vergeten!
       </p>
     </div>
   );
