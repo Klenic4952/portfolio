120130407cfc9ff5acf8879ecda0991c9d631606
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 21:31:28 +0200

    Add span and replace hover

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,21 +2,27 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav class=" lg:block" aria-label="In-page-jump-links">
+    <nav class="lg:block">
       <ul class="mt-16 w-max">
-        <li class="hover:font-bold">
+        <li>
           <Link href="#about" class="group flex items-center py-3 active">
-            OVER MIJ
+            <span class="group-hover:font-bold">
+              OVER MIJ
+              </span>
           </Link>
         </li>
         <li class="hover:font-bold">
           <Link href="#projects" class="group flex items-center py-3 active">
+          <span class="group-hover:font-bold">
             PROJECTEN
+            </span>
           </Link>
         </li>
         <li class="hover:font-bold">
           <Link href="#experience" class="group flex items-center py-3 active">
+          <span class="group-hover:font-bold">
             WERKERVARING
+            </span>
           </Link>
         </li>
       </ul>
