c80a03a269ed6fb15382bab020e66d8ccd597519
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 17:14:10 +0200

    Change text

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,34 +1,34 @@
 export default function About() {
   return (
     <div class="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20">
-      <h2>About</h2>
+      <h2 class="lg:sr-only">OVER MIJ</h2>
       <p class="mb-4">
-        Na een loopbaan van bijna 25 jaar bij de Rabobank was het tijd voor wat
+        Na een loopbaan van bijna 25 jaar als adviseur in de financiële dienstverlening was het tijd voor wat
         anders. Ik startte mijn eigen praktijk als voedingscoach. Met behulp van
-        WordPress maakte ik mijn eigen website. Dit wekte een nieuwe interesse
-        tot leven. Voor ik het wist had ik een opleiding tot Front-End
-        Development geboekt bij Winc Academy.
+        WordPress maakte ik de website voor mijn praktijk SMART Voedingsadvies. Dit wekte een nieuwe interesse
+        tot leven. Voor ik het wist had ik de opleiding Front-End Development geboekt bij Winc Academy.
       </p>
       <p class="mb-4">
-        Nadat ik de opleiding in mei 2024 had afgerond, heb ik dit portfolio
+        Na het behalen van mijn certificaat in mei 2024, heb ik dit portfolio
         gemaakt. Op dit moment werk ik aan een nieuwe website voor mijn
         praktijk. Zodra deze klaar is, voeg ik deze natuurlijk ook toe aan mijn
-        portfolio.Op dit moment besteed ik bij het maken van websites altijd
-        veel aandacht aan het kleurgebruik. Ik maak daarbij bijvoorbeeld gebruik
+        portfolio. Bij het maken van websites gaat mijn aandacht vaak uit
+        naar het kleurgebruik. Ik maak daarbij bijvoorbeeld gebruik
         van http://colormind.io/ en https://coolors.co/.
       </p>
       <p class="mb-4">
-        Zowel voeding als coderen en tech in het algemeen hebben mijn bijzondere
-        interesse. Ik ben benieuwd waar deze interesses me verder nog gaan
-        brengen.
+        De wereld bevind zich in mijn ervaring momenteel in een interessante transitie. 
+        Deze transitie, en dan wat specifieker, de transitie naar een gezond en 
+        duurzaam eetpatroon, maar ook de ontwikkelingen rondom digtitalisering
+        hebben mijn interesse.
       </p>
       <p class="mb-4">
-        Tijd om de hele dag achter de computer te zitten heb ik ook weer niet.
-        Ik run ook nog een ander bedrijf: mijn gezin. Met een man met een eigen
-        bedrijf, twee tieners en een kat: never al dull moment. Ik houd van de
-        natuur en daar wonen we dichtbij. Ik wandel graag, dat is een fijne
-        combinatie om lekker te bewegen en te ontspannen. En lekker eten niet te
-        vergeten!
+        Maar er zijn ook andere dingen die mijn aandacht nodig hebben.
+        Er is nog een ander leuk bedrijf te runnen: mijn gezin. Met een man met een eigen
+        bedrijf, twee tieners en een kat: never al dull moment. 
+        Ik houd van de natuur en daar wonen we dichtbij. Ik wandel graag, dat is een fijne
+        combinatie om lekker te bewegen en te ontspannen. En goed en lekker eten 
+        niet te vergeten!
       </p>
     </div>
   );
