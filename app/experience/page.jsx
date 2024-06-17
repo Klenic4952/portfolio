d45489a8f0023abbd71f72c86ac0d768e7806e3e
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 17:12:49 +0200

    Add page Experience

---
+++ app/experience/page.jsx
@@ -0,0 +1,10 @@
+export default function Experience() {
+    return (
+      <div class="bg-[#1B0F1C] p-10 rounded-xl">
+        <h2>WERKERVARING</h2>
+        <p class="text-[#987E9E]">
+          Werkervaring
+        </p>
+      </div>
+    );
+  }
