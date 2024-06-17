13a8d0b0c01938dac5731e85ffb1f96cd674228a
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 21:32:27 +0200

    Remove aria-label

--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,13 @@ import Projects from "./projects/page";
 export default function Home() {
   return (
     <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
-      <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="over mij">
+      <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
         <About />
       </section>
-      <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="projecten">
+      <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
         <Projects />
       </section>
-      <section id="experience" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="werkervaring">
+      <section id="experience" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
         <Experience/>
       </section>
     </main>
