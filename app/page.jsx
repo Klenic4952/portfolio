a38fc627bdb6f8e63395ae31d0d01e6caed8c6a7
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 17:14:34 +0200

    Add section Experience

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,20 @@
 // app/home.jsx is the UI for the '/' URL
 
 import About from "./about/page";
+import Experience from "./experience/page";
 import Projects from "./projects/page";
 
 export default function Home() {
   return (
     <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
-      <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="about me">
+      <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="over mij">
         <About />
       </section>
-      <section id="projects">
+      <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="projecten">
         <Projects />
+      </section>
+      <section id="experience" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"aria-label="werkervaring">
+        <Experience/>
       </section>
     </main>
   );
