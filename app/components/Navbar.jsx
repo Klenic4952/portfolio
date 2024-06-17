05108c25e2f06d9420055a69fd98df779a4e17a0
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-17 17:13:36 +0200

    Add hover to nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,14 +4,19 @@ export default function Navbar() {
   return (
     <nav class=" lg:block" aria-label="In-page-jump-links">
       <ul class="mt-16 w-max">
-        <li>
+        <li class="hover:font-bold">
           <Link href="#about" class="group flex items-center py-3 active">
-           OVER
+            OVER MIJ
           </Link>
         </li>
-        <li>
-          <Link href="#projects" class="font-bold">
+        <li class="hover:font-bold">
+          <Link href="#projects" class="group flex items-center py-3 active">
             PROJECTEN
+          </Link>
+        </li>
+        <li class="hover:font-bold">
+          <Link href="#experience" class="group flex items-center py-3 active">
+            WERKERVARING
           </Link>
         </li>
       </ul>
