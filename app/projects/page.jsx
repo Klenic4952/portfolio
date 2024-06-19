698467e517b3ec0ac4b823f149ba7d3b959e20c3
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-19 18:04:39 +0200

    Add dummy text for scroll

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,10 +1,32 @@
 export default function Projects() {
-    return (
-      <div class="bg-[#1B0F1C] p-10 rounded-xl">
-        <h2>PROJECTEN</h2>
-        <p class="text-[#987E9E]">
-          PROJECTEN
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div class="bg-[#1B0F1C] p-10 rounded-xl">
+      <h2>PROJECTEN</h2>
+      <p class="text-[#987E9E]">PROJECTEN</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+    </div>
+  );
+}
