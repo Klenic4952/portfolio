694311787db83b4b6eabb58e0d83f6e58307d6bb
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-20 17:27:26 +0200

    Add dummy-text

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -3,8 +3,30 @@ export default function Experience() {
       <div className="bg-[#1B0F1C] p-10 rounded-xl">
         <h2>WERKERVARING</h2>
         <p className="text-[#987E9E]">
-          Werkervaring
-        </p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestiae
+        ut unde alias? Molestias perspiciatis dolorem dolorum consequatur ab
+        voluptatum, commodi magnam adipisci, tempore repellat vitae quibusdam
+        dicta nam exercitationem!
+      </p>
+      
       </div>
     );
   }
