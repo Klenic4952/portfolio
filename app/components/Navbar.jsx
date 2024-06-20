6417440f70f08aab51c46bfc4ba0e367b32c86d6
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-20 17:29:10 +0200

    Add Icon to nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,29 +1,50 @@
-"use client"
+"use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import useActiveSection from "../hooks/useActiveSection";
+import { FaCircle } from "react-icons/fa";
 
-export default function Navbar() {
-  
-  
+const NavBar = () => {
+  const activeSection = useActiveSection();
+
   return (
     <nav className="lg:block">
       <ul className="mt-16 w-max">
         <li>
-          <Link href="#about" passHref className="group flex items-center py-3" >
+          <Link
+            href="#about"
+            className={`group flex items-center py-3 ${
+              activeSection === "about" ? "font-bold" : ""
+            }`}
+          >
+            {activeSection === "about" && <FaCircle className="fill-midpurple mr-3" />}
             <span className="group-hover:font-bold">OVER MIJ</span>
           </Link>
         </li>
         <li className="hover:font-bold">
-          <Link href="#projects" className="group flex items-center py-3" >
-            <span className="group-hover:font-bold">PROJECTEN</span>
+          <Link
+            href="#projects"
+            className={`group flex items-center py-3 ${
+              activeSection === "projects" ? "font-bold" : ""
+            }`}
+          >
+            {activeSection === "projects" && <FaCircle className="fill-midpurple mr-3"/>}
+           <span className="group-hover:font-bold">PROJECTEN</span>
           </Link>
         </li>
         <li className="hover:font-bold">
-          <Link href="#experience" className="group flex items-center py-3 active" >
+          <Link
+            href="#experience"
+            className={`group flex items-center py-3 ${
+              activeSection === "experience" ? "font-bold" : ""
+            }`}
+          >
+            {activeSection === "experience" && <FaCircle className="fill-midpurple mr-3"/>}
             <span className="group-hover:font-bold">WERKERVARING</span>
           </Link>
         </li>
       </ul>
     </nav>
   );
-}
+};
+
+export default NavBar;
