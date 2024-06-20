bfab94e90a4a2ef29d6e0629744205bd6044c15d
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-20 17:27:04 +0200

    Add ActiveSection hook

---
+++ app/hooks/useActiveSection.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+
+const useActiveSection = () => {
+  const [activeSection, setActiveSection] = useState("about");
+
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.6, // Adjust this value to determine when a section is considered "active"
+    });
+
+    const sections = document.querySelectorAll("main section");
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
+  return activeSection;
+};
+
+export default useActiveSection;
