3314384a27da2eccad24055a788d3ec0150e2900
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-24 14:04:54 +0200

    Edit text about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,34 +1,31 @@
 export default function About() {
   return (
-    <div className="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20">
+    <div className="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20 text-[17px]">
       <h2 className="lg:sr-only">OVER MIJ</h2>
       <p className="mb-4">
-        Na een loopbaan van bijna 25 jaar als adviseur in de financiële dienstverlening was het tijd voor wat
-        anders. Ik startte mijn eigen praktijk als voedingscoach. Met behulp van
-        WordPress maakte ik de website voor mijn praktijk SMART Voedingsadvies. Dit wekte een nieuwe interesse
-        tot leven. Voor ik het wist had ik de opleiding Front-End Development geboekt bij Winc Academy.
+        Na een loopbaan van bijna 25 jaar in de financiële dienstverlening
+        was het tijd voor wat anders. Ik startte mijn eigen praktijk als voedingscoach. 
+        Met behulp van WordPress maakte ik de website voor mijn praktijk SMART Voedingsadvies.
+        Dit wekte een nieuwe interesse tot leven. Voor ik het wist had ik de opleiding 
+        Front-End Development geboekt bij Winc Academy.
       </p>
       <p className="mb-4">
         Na het behalen van mijn certificaat in mei 2024, heb ik dit portfolio
-        gemaakt. Op dit moment werk ik aan een nieuwe website voor mijn
-        praktijk. Zodra deze klaar is, voeg ik deze natuurlijk ook toe aan mijn
-        portfolio. Bij het maken van websites gaat mijn aandacht vaak uit
-        naar het kleurgebruik. Ik maak daarbij bijvoorbeeld gebruik
-        van http://colormind.io/ en https://coolors.co/.
+        gemaakt met Next.js en Tailwind. Daarna heb ik in WordPress de website
+        voor mijn praktijk opgefrist. Ook deze vind je in dit portfolio. Bij het
+        maken van websites gaat mijn aandacht vaak uit naar het kleurgebruik.
+        Ik maak daarbij op dit moment gebruik van http://colormind.io/ en https://coolors.co/.
       </p>
       <p className="mb-4">
-        De wereld bevind zich in mijn ervaring momenteel in een interessante transitie. 
-        Deze transitie, en dan wat specifieker, de transitie naar een gezond en 
-        duurzaam eetpatroon, maar ook de ontwikkelingen rondom digitalisering
-        hebben mijn interesse.
+        Naast mijn interesse voor coderen en eten, kan ik 
+        erg genieten van de natuur. Ik woon met mijn gezin dichtbij 
+        mooie natuurgebieden. Daar maak ik heel wat wandelkilometers. 
+        Een fijne manier om te bewegen en te ontspannen.         
       </p>
-      <p className="mb-4">
-        Maar er zijn ook andere dingen die mijn aandacht nodig hebben.
-        Er is nog een ander leuk bedrijf te runnen: mijn gezin. Met een man met een eigen
-        bedrijf, twee tieners en een kat: never al dull moment. 
-        Ik houd van de natuur en daar wonen we dichtbij. Ik wandel graag, dat is een fijne
-        combinatie om lekker te bewegen en te ontspannen. En goed en lekker eten 
-        niet te vergeten!
+      <p>
+        Ik sluit niet uit dat ik naast mijn praktijk SMART Voedingsadvies, ook 
+        aan de slag ga met het maken van websites. De komende tijd ga ik 
+        daar verder mee pionieren. 
       </p>
     </div>
   );
