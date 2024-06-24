291ceea8e9d0f5967f8390b57d039a0606be3496
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-24 14:05:19 +0200

    Shuffled order pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,20 @@
 // app/home.jsx is the UI for the '/' URL
 
 import About from "./about/page";
-import Experience from "./experience/page";
+import Education from "./education/page";
 import Projects from "./projects/page";
 
 export default function Home() {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
-      <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+      <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
         <About />
       </section>
-      <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
-        <Projects />
+      <section id="education" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
+        <Education/>
       </section>
-      <section id="experience" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
-        <Experience/>
+      <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
+        <Projects />
       </section>
     </main>
   );
