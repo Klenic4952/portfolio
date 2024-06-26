4775bdde054636ea6351ba82c555ff7deb321be8
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-06-26 13:46:53 +0200

    Move icons to education

---
+++ app/education/icons.tsx
@@ -0,0 +1,28 @@
+import { FaHtml5, FaCss3Alt, FaSass, FaReact, FaGithub, FaGitSquare, FaFigma, FaNpm, FaNode, FaWordpress } from "react-icons/fa";
+import { SiJavascript, SiChakraui, SiPowershell, SiWindows, SiPostman, SiTailwindcss, SiNetlify } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
+import { PiDevicesLight } from "react-icons/pi";
+import { RiNextjsFill } from "react-icons/ri";
+
+export const Icons = {
+    html5: FaHtml5,
+    css: FaCss3Alt,
+    sass: FaSass,
+    react: FaReact,
+    github: FaGithub,
+    git: FaGitSquare,
+    figma: FaFigma,
+    npm: FaNpm,
+    node: FaNode,
+    wordpress: FaWordpress,
+    javascript: SiJavascript,
+    chakra: SiChakraui,
+    powershell: SiPowershell,
+    windows: SiWindows,
+    postman: SiPostman,
+    tailwind: SiTailwindcss,
+    netlify: SiNetlify,
+    vscode: VscVscode,
+    rwd: PiDevicesLight,
+    next: RiNextjsFill,
+}
