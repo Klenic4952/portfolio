75a63e36e9cd69daeb2b4647ec35ecd903fccb91
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-03 17:33:27 +0200

    Edit text About

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,29 +3,35 @@ export default function About() {
     <div className="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20 text-[17px]">
       <h2 className="lg:sr-only">OVER MIJ</h2>
       <p className="mb-4">
-        Na een loopbaan van bijna 25 jaar in de financiële dienstverlening
-        was het tijd voor wat anders. Ik startte mijn eigen praktijk als voedingscoach. 
-        Met behulp van WordPress maakte ik de website voor mijn praktijk SMART Voedingsadvies.
-        Dit wekte een nieuwe interesse tot leven. Voor ik het wist had ik de opleiding 
-        Front-End Development geboekt bij Winc Academy.
+        Na een loopbaan van bijna 25 jaar in de financiële dienstverlening, startte ik mijn 
+        eigen praktijk als voedingscoach. Met behulp van WordPress maakte ik de website voor
+        mijn praktijk SMART Voedingsadvies. Dit wekte een nieuwe interesse tot leven. Voor ik
+        het wist had ik de opleiding Front-End Development geboekt bij Winc Academy.
       </p>
       <p className="mb-4">
-        Na het behalen van mijn certificaat in mei 2024, heb ik dit portfolio
+        Beide keuzes waren overigens ook weer niet 180 graden anders dan voorheen. Ik werkte als 
+        financieel adviseur dagelijks met klanten. Daarnaast heeft digitalisering mijn interesse en gaat 
+        het me ook makkelijk af. Ik was handig met, regelmatig nieuwe, systemen en hielp daarbij ook 
+        nog wel eens een collega.
+      </p>
+      <p className="mb-4">
+        Na het behalen van mijn certificaat in mei 2024 bij Winc Academy, heb ik dit portfolio
         gemaakt met Next.js en Tailwind. Daarna heb ik in WordPress de website
         voor mijn praktijk opgefrist. Ook deze vind je in dit portfolio. Bij het
         maken van websites gaat mijn aandacht vaak uit naar het kleurgebruik.
         Ik maak daarbij op dit moment gebruik van http://colormind.io/ en https://coolors.co/.
       </p>
       <p className="mb-4">
-        Naast mijn interesse voor coderen en eten, kan ik 
+        Naast mijn interesse voor coderen, digitalisering en eten, kan ik 
         erg genieten van de natuur. Ik woon met mijn gezin dichtbij 
         mooie natuurgebieden. Daar maak ik heel wat wandelkilometers. 
         Een fijne manier om te bewegen en te ontspannen.         
       </p>
       <p>
         Ik sluit niet uit dat ik naast mijn praktijk SMART Voedingsadvies, ook 
-        aan de slag ga met het maken van websites. De komende tijd ga ik 
-        daar verder mee pionieren. 
+        aan de slag ga met het maken van websites. Of wat er anders maar op mijn pad
+        komt op het gebied van digitalisering. De komende tijd ga ik daar verder 
+        mee pionieren. 
       </p>
     </div>
   );
