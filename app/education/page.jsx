7603e59cb7f72d8fdc9df8eefb692b0be43f947f
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-03 17:33:07 +0200

    Add SkillsList to page Education

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import Certificate from "./certificate.png";
-import Skills from "../components/Skills"
+import SkillsList from "./SkillsList";
 
 export default function Education() {
   return (
     <div className="bg-midpurple p-10 rounded-xl text-[17px]">
       <h2 className="lg:sr-only">OPLEIDING</h2>
       <p className="text-purpledark">
-        In mei 2024 slaagde ik voor de opleiding Front-end Development bij
-        Winc Academy. Check hieronder mijn Front-end certificaat.
+        In mei 2024 slaagde ik voor de opleiding Front-end Development bij Winc
+        Academy. Check hieronder mijn Front-end certificaat.
       </p>
       <p className="mt-10">
-      <Link href="https://certificates.wincacademy.com/655599e5-d0a0-4c5a-8377-353d4589c4d5#gs.9i91ce" target="blank">
-        <Image src={Certificate} alt="Certificate Front-end Development"  />
-      </Link>
+        <Link
+          href="https://certificates.wincacademy.com/655599e5-d0a0-4c5a-8377-353d4589c4d5#gs.9i91ce"
+          target="blank"
+        >
+          <Image
+            src={Certificate}
+            alt="Certificate Front-end Development"
+            quality={100}
+          />
+        </Link>
       </p>
-      
-      <Skills/>
-      </div>
+      <SkillsList/>
+    </div>
   );
 }
