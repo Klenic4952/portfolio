f4316a8dc9e842fdd9d97d8014ba7124b8308651
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-08 16:28:06 +0200

    Create Suspense boundarie

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,34 @@
 // app/home.jsx is the UI for the '/' URL
 
+import { Suspense } from "react";
 import About from "./about/page";
 import Education from "./education/page";
 import Projects from "./projects/page";
+import Loading from "./loading";
 
 export default function Home() {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
-      <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
+      <section
+        id="about"
+        className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+      >
         <About />
       </section>
-      <section id="education" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
-        <Education/>
-      </section>
-      <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24">
-        <Projects />
-      </section>
+      <Suspense fallback={<Loading />}>
+        <section
+          id="education"
+          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+        >
+          <Education />
+        </section>
+        <section
+          id="projects"
+          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+        >
+          <Projects />
+        </section>
+      </Suspense>
     </main>
   );
 }
