75413bee97debcc500f8107523ad5285ccd26468
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-08 21:01:51 +0200

    Edit text

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,38 +1,38 @@
 export default function About() {
   return (
-    <div className="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20 text-[15px]">
+    <div className="bg-[#DBAA78] p-10 rounded-xl sticky top-0 z-20 text-[17px]">
       <h2 className="lg:sr-only">OVER MIJ</h2>
-      <p className="mb-4">
-        Na een loopbaan van bijna 25 jaar in de financiële dienstverlening, startte ik een 
-        praktijk als voedingscoach. Met behulp van WordPress maakte ik de website voor
-        mijn praktijk SMART Voedingsadvies. Dit wekte een nieuwe interesse tot leven. Voor ik
-        het wist had ik de opleiding Front-End Development geboekt bij Winc Academy.
+      <div className="rounded-xl border-solid border-4 border- border-beige">
+      <p className="mb-4 pt-5 px-3">
+        Na een loopbaan van bijna 25 jaar als adviseur in de financiële dienstverlening, startte 
+        ik een praktijk als voedingscoach. Met WordPress maakte ik de website voor
+        mijn praktijk SMART Voedingsadvies. Dit vond ik zo boeiend, voor ik het wist had
+        ik bij Winc Academy de opleiding Front-End Development geboekt.
       </p>
-      <p className="mb-4">
-        Beide keuzes waren ook weer niet 180 graden anders dan voorheen. Ik werkte als 
-        financieel adviseur dagelijks met klanten. Daarnaast heeft digitalisering mijn interesse en gaat 
-        het me makkelijk af. Ik vind snel mijn weg met software en andere systemen en was daarom
-        soms ook vraagbaak voor anderen. Wat daarnaast mijn aandacht heeft, zijn de 
-        duurzaamheidsvraagstukken die er zijn voor zowel voeding als digitalisering. 
+      <p className="mb-4 px-3">
+       In het eerste opzicht lijken het 180 graden andere keuzes, maar qua vaardigheden en 
+       competenties valt dat wat wel mee. Ik werkte als financieel adviseur
+       ook met klanten. Daarnaast vond ik altijd snel mijn weg in systemen en software. 
       </p>
-      <p className="mb-4">
+      <p className="mb-4 px-3">
         Na het behalen van mijn certificaat in mei 2024 bij Winc Academy, heb ik dit portfolio
         gemaakt met Next.js en Tailwind. Daarna heb ik in WordPress de website
         voor mijn praktijk opgefrist. Ook deze vind je in dit portfolio. Bij het
         maken van websites gaat mijn aandacht vaak uit naar het kleurgebruik.
         Ik maak daarbij op dit moment gebruik van http://colormind.io/ en https://coolors.co/.
       </p>
-      <p className="mb-4">
+      <p className="mb-4 px-3">
         Naast mijn interesse voor coderen, digitalisering en eten, kan ik 
         erg genieten van de natuur. Ik woon met mijn gezin dichtbij 
         mooie natuurgebieden. Daar maak ik heel wat wandelkilometers. 
         Een fijne manier om te bewegen en te ontspannen.         
       </p>
-      <p>
+      <p className=" mb-4 px-3">
         Ik sluit niet uit dat ik naast mijn praktijk SMART Voedingsadvies, ook 
         aan de slag ga met het maken van websites. Of wat er anders maar op mijn pad
         komt. De komende tijd ga ik daar verder mee pionieren. 
       </p>
+      </div>
     </div>
   );
 }
