364b22db5bc54e5ae5ecd06488c59b046762043a
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-10 18:08:25 +0200

    Add dynamic import

--- app/education/icons.tsx
+++ app/education/icons.tsx
@@ -1,8 +1,27 @@
-import { FaHtml5, FaCss3Alt, FaSass, FaReact, FaGithub, FaGitSquare, FaFigma, FaNpm, FaNode, FaWordpress } from "react-icons/fa";
-import { SiJavascript, SiChakraui, SiPowershell, SiWindows, SiPostman, SiTailwindcss, SiNetlify } from "react-icons/si";
-import { VscVscode } from "react-icons/vsc";
-import { PiDevicesLight } from "react-icons/pi";
-import { RiNextjsFill } from "react-icons/ri";
+import dynamic from 'next/dynamic';
+
+const FaHtml5 = dynamic(() => import("react-icons/fa").then(mod => mod.FaHtml5));
+const FaCss3Alt = dynamic(() => import("react-icons/fa").then(mod => mod.FaCss3Alt));
+const FaSass = dynamic(() => import("react-icons/fa").then(mod => mod.FaSass));
+const FaReact = dynamic(() => import("react-icons/fa").then(mod => mod.FaReact));
+const FaGithub = dynamic(() => import("react-icons/fa").then(mod => mod.FaGithub));
+const FaGitSquare = dynamic(() => import("react-icons/fa").then(mod => mod.FaGitSquare));
+const FaFigma = dynamic(() => import("react-icons/fa").then(mod => mod.FaFigma));
+const FaNpm = dynamic(() => import("react-icons/fa").then(mod => mod.FaNpm));
+const FaNode = dynamic(() => import("react-icons/fa").then(mod => mod.FaNode));
+const FaWordpress = dynamic(() => import("react-icons/fa").then(mod => mod.FaWordpress));
+
+const SiJavascript = dynamic(() => import("react-icons/si").then(mod => mod.SiJavascript));
+const SiChakraui = dynamic(() => import("react-icons/si").then(mod => mod.SiChakraui));
+const SiPowershell = dynamic(() => import("react-icons/si").then(mod => mod.SiPowershell));
+const SiWindows = dynamic(() => import("react-icons/si").then(mod => mod.SiWindows));
+const SiPostman = dynamic(() => import("react-icons/si").then(mod => mod.SiPostman));
+const SiTailwindcss = dynamic(() => import("react-icons/si").then(mod => mod.SiTailwindcss));
+const SiNetlify = dynamic(() => import("react-icons/si").then(mod => mod.SiNetlify));
+
+const VscVscode = dynamic(() => import("react-icons/vsc").then(mod => mod.VscVscode));
+const PiDevicesLight = dynamic(() => import("react-icons/pi").then(mod => mod.PiDevicesLight));
+const RiNextjsFill = dynamic(() => import("react-icons/ri").then(mod => mod.RiNextjsFill));
 
 export const Icons = {
     html5: FaHtml5,
@@ -25,4 +44,4 @@ export const Icons = {
     vscode: VscVscode,
     rwd: PiDevicesLight,
     next: RiNextjsFill,
-}
+};
