02ebf4857d146ac795697c17c979cf2ca579c90c
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-11 14:46:25 +0200

    Remove Head tag (to metadata)

--- app/page.jsx
+++ app/page.jsx
@@ -11,62 +11,37 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <Link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <Link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <Link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <Link rel="manifest" href="/site.webmanifest" />
-        <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1b0f1c" />
-        <meta name="msapplication-TileColor" content="#603cba" />
-        <meta name="theme-color" content="#fff1dd" />
-      </Head>
-      <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
-        <section
-          id="about"
-          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
-        >
-          <About />
-        </section>
-        <section
-          id="education"
-          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
-        >
-          <Suspense fallback={<Loading />}>
-            <Education />
-          </Suspense>
-        </section>
-        <section
-          id="projects"
-          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
-        >
-          <Suspense fallback={<Loading />}>
-            <Projects />
-          </Suspense>
-        </section>
-        <section
-          id="contact"
-          className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
-        >
-          <Suspense fallback={<Loading />}>
-            <Contact />
-          </Suspense>
-        </section>
-      </main>
-    </>
+    <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+      <section
+        id="about"
+        className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+      >
+        <About />
+      </section>
+      <section
+        id="education"
+        className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+      >
+        <Suspense fallback={<Loading />}>
+          <Education />
+        </Suspense>
+      </section>
+      <section
+        id="projects"
+        className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+      >
+        <Suspense fallback={<Loading />}>
+          <Projects />
+        </Suspense>
+      </section>
+      <section
+        id="contact"
+        className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
+      >
+        <Suspense fallback={<Loading />}>
+          <Contact />
+        </Suspense>
+      </section>
+    </main>
   );
 }
