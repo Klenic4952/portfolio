dc1028f32a4b2f3e6ffdc3662bd14b4752f72a07
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-07-11 17:16:46 +0200

    Add page Experience

---
+++ app/experience/page.jsx
@@ -0,0 +1,11 @@
+export default function Experience () {
+    return (
+      <div className="bg-brown p-10 rounded-xl text-[17px]">
+        <h2 className="lg:sr-only">WERKERVARING</h2>
+        <p className="text-purpledark">
+          Mijn werkzame leven heeft zich lange tijd afgespeeld bij de Rabobank. 
+          Check mijn CV voor mijn werkervaring tot nu toe. 
+        </p>
+        </div>
+    )
+}
