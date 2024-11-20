4b0704f3ffe10d6f48b24f4151862308296b1e00
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-11-20 15:51:17 +0100

    Change header on mobile

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,39 +1,45 @@
 export default function About() {
   return (
-    <div className="bg-[#DBAA78] p-8 rounded-xl sticky top-0 z-20 text-[17px]">
-      <h2 className="lg:sr-only pl-2 pb-2">OVER MIJ</h2>
+    <div className="bg-[#DBAA78] px-8 pb-8 lg:p-8 rounded-xl sticky top-0 z-20 text-[17px]">
+      <div className="sticky top-0 -mx-2 px-3 pt-5 pb-1 mb-4 bg-[#DBAA78] border-b-2 border-black md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="lg:sr-only pl-2 pb-2">OVER MIJ</h2>
+      </div>
       <div className="p-2 rounded-xl border-solid border-4 border- border-beige">
-      <p className="mb-4 pt-2 px-3">
-        Na een loopbaan van bijna 25 jaar als adviseur in de financiële dienstverlening, startte 
-        ik een praktijk als gewichtsconsulent. Met WordPress maakte ik de website voor
-        mijn praktijk SMART Voedingsadvies. Dit vond ik zo boeiend, dat ik bij Winc Academy 
-        de opleiding Front-End Development heb gevolgd.
-      </p>
-      <p className="mb-4 px-3">
-       In het eerste opzicht lijkt dat een 180 graden andere keuze, maar qua vaardigheden en 
-       competenties past het allebei bij mij. Ik vond het als financieel adviseur
-       altijd erg leuk om klanten te adviseren. Daarbij vond ik snel mijn weg in systemen en software, 
-       wat me in de financiele dienstverlening ook goed van pas kwam.  
-      </p>
-      <p className="mb-4 px-3">
-        Na het behalen van mijn certificaat in mei 2024 bij Winc Academy, heb ik dit portfolio
-        gemaakt met Next.js en Tailwind. Daarna heb ik in WordPress de website
-        voor mijn praktijk opnieuw gebouwd met WordPress en Elementor. Ook deze vind je in dit portfolio. 
-        Bij het maken van websites gaat mijn aandacht vaak uit naar het kleurgebruik.
-        Ik maak daarbij gebruik van http://colormind.io/ en https://coolors.co/.
-      </p>
-      <p className="mb-4 px-3">
-        Naast mijn interesse voor coderen, digitalisering en eten, kan ik 
-        erg genieten van de natuur. Ik woon met mijn gezin dichtbij 
-        de natuur en wandel zo de Drentse heide op. Wandelen is voor mij een fijne manier 
-        om te bewegen en te ontspannen.         
-      </p>
-      <p className=" mb-4 px-3">
-        Wat werken voor mij nog steeds het leukst maakt, is om ingewikkelde dingen voor mensen 
-        makkelijker te maken. ICT is daarbij voor mij een nieuw werkveld waar ik met veel interesse 
-        kennis mee gemaakt heb. Het is leuk om mensen te helpen om een mooie website te realiseren. 
-        Maar ik zou ook goed op mijn plek zijn in een ICT-Servicedesk omgeving.
-      </p>
+        <p className="mb-4 pt-2 px-3">
+          Na een loopbaan van bijna 25 jaar als adviseur in de financiële
+          dienstverlening, startte ik een praktijk als gewichtsconsulent. Met
+          WordPress maakte ik de website voor mijn praktijk SMART
+          Voedingsadvies. Dit vond ik zo boeiend, dat ik bij Winc Academy de
+          opleiding Front-End Development heb gevolgd.
+        </p>
+        <p className="mb-4 px-3">
+          In het eerste opzicht lijkt dat een 180 graden andere keuze, maar qua
+          vaardigheden en competenties past het allebei bij mij. Ik vond het als
+          financieel adviseur altijd erg leuk om klanten te adviseren. Daarbij
+          vond ik snel mijn weg in systemen en software, wat me in de financiele
+          dienstverlening ook goed van pas kwam.
+        </p>
+        <p className="mb-4 px-3">
+          Na het behalen van mijn certificaat in mei 2024 bij Winc Academy, heb
+          ik dit portfolio gemaakt met Next.js en Tailwind. Daarna heb ik in
+          WordPress de website voor mijn praktijk opnieuw gebouwd met WordPress
+          en Elementor. Ook deze vind je in dit portfolio. Bij het maken van
+          websites gaat mijn aandacht vaak uit naar het kleurgebruik. Ik maak
+          daarbij gebruik van http://colormind.io/ en https://coolors.co/.
+        </p>
+        <p className="mb-4 px-3">
+          Naast mijn interesse voor coderen, digitalisering en eten, kan ik erg
+          genieten van de natuur. Ik woon met mijn gezin dichtbij de natuur en
+          wandel zo de Drentse heide op. Wandelen is voor mij een fijne manier
+          om te bewegen en te ontspannen.
+        </p>
+        <p className=" mb-4 px-3">
+          Wat werken voor mij nog steeds het leukst maakt, is om ingewikkelde
+          dingen voor mensen makkelijker te maken. ICT is daarbij voor mij een
+          nieuw werkveld waar ik met veel interesse kennis mee gemaakt heb. Het
+          is leuk om mensen te helpen om een mooie website te realiseren. Maar
+          ik zou ook goed op mijn plek zijn in een ICT-Servicedesk omgeving.
+        </p>
       </div>
     </div>
   );
