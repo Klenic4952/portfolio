a40c6627453d99b13ce0f539752a79907c82be7f
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-11-20 16:02:53 +0100

    Change padding main on mobile

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ import Loading from "./loading";
 
 export default function Home() {
   return (
-    <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+    <main id="content" className="pt-14 lg:pt-24 lg:w-1/2 lg:py-24">
       <section
         id="about"
         className="mb-16 scroll-mt-16 md:mb-24 lg:scroll-mt-24"
