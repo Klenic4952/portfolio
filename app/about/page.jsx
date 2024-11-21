406392ec67fe7c2671f05802617e8a4b9d62e71d
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-11-21 13:40:32 +0100

    Change padding mobile

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,18 +1,18 @@
 export default function About() {
   return (
-    <div className="bg-[#DBAA78] px-8 p-8 pt-5 lg:p-10 rounded-xl text-[17px]">
-      <div className="sticky top-0 -mx-2 px-3 pt-5 pb-2 mb-4 bg-[#DBAA78] border-b-2 border-black md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+    <div className="bg-[#DBAA78] px-4 lg:px-8 p-8 pt-5 lg:p-10 rounded-xl text-[17px]">
+      <div className="sticky top-0 -mx-2 px-2 pt-5 pb-2 mb-4 bg-[#DBAA78] border-b-2 border-black md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="lg:sr-only pl-2 pb-2">OVER MIJ</h2>
       </div>
       <div className="p-2 rounded-xl border-solid border-4 border-beige">
-        <p className="mb-4 pt-2 px-3">
+        <p className="mb-4 pt-2 px-2 lg:px-3">
           Na een loopbaan van bijna 25 jaar als adviseur in de financiële
           dienstverlening, besloot ik mijn passie te volgen en startte ik een
           praktijk als Gewichtsconsulent. Voor mijn praktijk, SMART Voedingsadvies,
           creëerde ik een website met WordPress. Dit vond ik zo boeiend dat ik besloot
           een opleiding Front-End Development te volgen bij Winc Academy.
         </p>
-        <p className="mb-4 px-3">
+        <p className="mb-4 px-2 lg:px-3">
           Op het eerste gezicht lijkt dit misschien een drastische carrièreswitch, 
           maar de vaardigheden en competenties overlappen meer dan je zou denken.
           Als financieel adviseur haalde ik veel voldoening uit het helpen van klanten 
@@ -23,19 +23,19 @@ export default function About() {
           Zo ontwierp ik niet alleen een website, maar maakte ik ook gebruik van een 
           online intakeformulier, online agenda en nieuwsbrief.
         </p>
-        <p className="mb-4 px-3">
+        <p className="mb-4 px-2 lg:px-3">
           In mei 2024 haalde ik mijn certificaat Front-End Development 
           bij Winc Academy.Daarna maakte ik dit portfolio met Next.js en Tailwind. 
           Vervolgens heb ik de website voor mijn praktijk opnieuw opgebouwd
           met WordPress en Elementor, die ook in dit portfolio te vinden is.
         </p>
-        <p className="mb-4 px-3">
+        <p className="mb-4 px-2 lg:px-3">
           Naast mijn interesse in het coachen en begeleiden van mensen, (gezond)
           eten en ICT, geniet ik enorm van de natuur. Ik woon met mijn gezin 
           dichtbij de natuur en wandel regelmatig op de Drentse heide. Wandelen 
           is voor mij een geweldige manier om te bewegen en te ontspannen.
         </p>
-        <p className=" mb-4 px-3">
+        <p className=" mb-4 px-2 lg:px-3">
           Wat ik het leukst vind in mijn werk, is ingewikkelde
           zaken eenvoudiger maken voor mensen. ICT is voor mij een
           nieuw werkveld waarmee ik met veel interesse kennis heb gemaakt. 
@@ -43,10 +43,8 @@ export default function About() {
           relatie met eten en zichzelf ontwikkelen. Daarnaast lijkt het me ook leuk 
           om anderen te helpen met het realiseren van een mooie website. Een rol op een 
           ICT-gerelateerde Servicedesk of als Digicoach zou ook goed bij me passen.
-          Kort samengevat: ik ben een combinatie van sociale en communicatieve vaardigheden
-          met een analytische en probleemoplossende aanpak en de nodige vasthoudendheid.
         </p>
-        <p className=" mb-4 px-3">
+        <p className=" mb-4 px-2 lg:px-3">
           Kort samengevat: je vindt in mij een combinatie van sociale en communicatieve vaardigheden
           met een analytische en probleemoplossende aanpak en de nodige vasthoudendheid.
         </p>
