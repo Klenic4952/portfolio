da952fbcafbd2cc907386efe61220017d202bd3a
NICOLIEN KLEIN <nicolienklein@live.nl>
2024-11-27 17:49:13 +0100

    Change order projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -11,6 +11,47 @@ export default function Projects() {
       </div>
       <div>
         <div>
+          <div className="mt-6">
+            <h3 className="text-midpurple font-bold text-center text-[24px]">
+              SMART Voedingsadvies
+            </h3>
+            <a href="https://smartvoedingsadvies.nl/" target="blank">
+              <Image
+                src={SmartWebsite}
+                alt="SMART Voedingsadvies"
+                width={550}
+                height={374}
+                className="border-solid border-8 border-midpurple rounded-lg mx-auto mt-10"
+                sizes="(min-width: 1340px) 325px, (min-width: 1040px) calc(30vw - 71px), (min-width: 780px) calc(70vw - 123px), calc(70vw - 90px)"
+                style={{
+                  width: "75%",
+                  height: "auto",
+                }}
+              />
+            </a>
+          </div>
+          <div>
+            <p className="text-beigelight my-8">
+              Deze website voor mijn praktijk SMART Voedingsadvies heb ik
+              gemaakt na het voltooien van de opleiding. Ik heb de website
+              gemaakt met {" "}<a href="https://wordpress.com/nl/" className="text-purplelight hover:text-beigelight" target="blank">
+                WordPress
+              </a>
+              {" "}en{" "}
+              <a href="https://elementor.com/" className="text-purplelight hover:text-beigelight" target="blank">
+                Elementor
+              </a>
+              . Als basis heb ik voor het{" "}
+              <a href="https://creativethemes.com/blocksy/" className="text-purplelight hover:text-beigelight" target="blank">
+                Blocksy-Theme
+              </a>{" "}
+              gekozen. Daarbij heb ik geen standaard templates gebruikt, maar
+              zelf de pagina`s opgebouwd. Waar nodig heb ik CSS naar eigen wens
+              aangepast.
+            </p>
+          </div>
+        </div>
+        <div>
           <h3 className="text-midpurple font-bold text-center text-[24px]">
             Event App
           </h3>
@@ -23,7 +64,7 @@ export default function Projects() {
               className="border-solid border-8 border-midpurple rounded-lg mx-auto mt-10"
               sizes="(min-width: 1340px) 286px, (min-width: 1040px) calc(27.86vw - 82px), (min-width: 780px) calc(65vw - 130px), calc(65vw - 99px)"
               style={{
-                width: "65%",
+                width: "75%",
                 height: "auto",
               }}
             />
@@ -32,11 +73,22 @@ export default function Projects() {
         <div>
           <p className="text-beigelight my-8">
             Dit was het finale assessment voor de opleiding in 2024. De React
-            Applicatie is gemaakt met behulp van Vite.js, React Router en Chakra
-            UI. Het is ontworpen met als doel het zoeken van, bewerken,
-            toevoegen of verwijderen van evementen uit een database. Deze
-            applicatie is alleen voor demonstratie doeleinden, de database is
-            gekoppeld aan My JSON Server.
+            Applicatie is gemaakt met behulp van{" "}
+            <a href="https://vite.dev/" className="text-purplelight hover:text-beigelight" target="blank">
+              Vite.js
+            </a>
+            ,{" "}
+            <a href="https://reactrouter.com/" className="text-purplelight hover:text-beigelight" target="blank">
+              React Router</a> en{" "}
+              <a href="https://www.chakra-ui.com/" className="text-purplelight hover:text-beigelight" target="blank">Chakra UI
+            </a>
+            . Het is ontworpen met als doel het zoeken van, bewerken, toevoegen
+            of verwijderen van evementen uit een database. Deze applicatie is
+            alleen voor demonstratie doeleinden, de database is gekoppeld aan{" "}
+            <a href="https://my-json-server.typicode.com/" className="text-purplelight hover:text-beigelight" target="blank">
+              My JSON Server
+            </a>
+            .
           </p>
         </div>
       </div>
@@ -54,7 +106,7 @@ export default function Projects() {
               className="border-solid border-8 border-midpurple rounded-lg mx-auto mt-10"
               sizes="(min-width: 1340px) 325px, (min-width: 1040px) calc(30vw - 71px), (min-width: 780px) calc(70vw - 123px), calc(70vw - 90px)"
               style={{
-                width: "65%",
+                width: "75%",
                 height: "auto",
               }}
             />
@@ -63,45 +115,19 @@ export default function Projects() {
         <div>
           <p className="text-beigelight my-8">
             Deze app was een opdracht voor de opleiding eind 2023. Deze React
-            applicatie is gemaakt met behulp van React en Chakra UI. Het biedt
-            gebruikers de mogelijkheid om recepten te zoeken op basis van
-            dieet-voorkeuren, gezondheidslabels of de naam van het recept.
-            Wanneer de gebruiker een recept selecteert, wordt er gedetailleerde
-            informatie met betrekking tot het gekozen recept getoond.
+            applicatie is gemaakt met behulp van{" "}
+            <a href="https://react.dev/" className="text-purplelight hover:text-beigelight" target="blank">
+              React</a> en {" "}
+              <a href="https://www.chakra-ui.com/" className="text-purplelight hover:text-beigelight" target="blank">Chakra UI
+            </a>
+            . Het biedt gebruikers de mogelijkheid om recepten te zoeken op
+            basis van dieet-voorkeuren, gezondheidslabels of de naam van het
+            recept. Wanneer de gebruiker een recept selecteert, wordt er
+            gedetailleerde informatie met betrekking tot het gekozen recept
+            getoond.
           </p>
         </div>
       </div>
-      <div>
-        <div className="mt-12">
-          <h3 className="text-midpurple font-bold text-center text-[24px]">
-            SMART Voedingsadvies
-          </h3>
-          <a href="https://smartvoedingsadvies.nl/" target="blank">
-            <Image
-              src={SmartWebsite}
-              alt="SMART Voedingsadvies"
-              width={550}
-              height={374}
-              className="border-solid border-8 border-midpurple rounded-lg mx-auto mt-10"
-              sizes="(min-width: 1340px) 325px, (min-width: 1040px) calc(30vw - 71px), (min-width: 780px) calc(70vw - 123px), calc(70vw - 90px)"
-              style={{
-                width: "65%",
-                height: "auto",
-              }}
-            />
-          </a>
-        </div>
-        <div>
-          <p className="text-beigelight my-8">
-            Deze website voor mijn praktijk SMART Voedingsadvies heb ik gemaakt
-            na het voltooien van de opleiding. Ik heb de website gemaakt met WordPress 
-            en Elementor. Als basis heb ik voor het Blocksy-Theme gekozen. Daarbij 
-            heb ik uiteraard geen standaard templates gebruikt, maar zelf de 
-            pagina`s opgebouwd. Waar nodig heb ik de CSS naar eigen wens aangepast. 
-          </p>
-        </div>
-      </div>
-    
     </div>
   );
 }
